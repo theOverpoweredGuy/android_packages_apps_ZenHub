731c4a9e1da37d56ff21626e8c37ae2ce480af5f
Lokesh773 <lokeshkasamneni773@gmail.com>
2020-08-15 19:25:12 +0700

    Revert "Add Ambient Customizations [2/2]"
    This reverts commit c5ba052cbd0e0fab4fa82e910cc13d5fc6951eec.

--- src/com/zenx/zen/hub/fragments/lockscreenambient/tabs/Ambient.java
+++ src/com/zenx/zen/hub/fragments/lockscreenambient/tabs/Ambient.java
@@ -23,7 +23,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
 import android.widget.TextView;
-import android.content.pm.ResolveInfo;
 
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
@@ -32,12 +31,10 @@ import com.android.internal.logging.nano.MetricsProto;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 
 import com.zenx.support.colorpicker.ColorPickerPreference;
 import com.zenx.support.preferences.CustomSeekBarPreference;
 import com.zenx.support.preferences.SystemSettingSwitchPreference;
-import com.zenx.support.preferences.SystemSettingEditTextPreference;
 
 public class Ambient extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
@@ -48,28 +45,12 @@ public class Ambient extends SettingsPreferenceFragment implements
     private static final String AMBIENT_LIGHT_DURATION = "ambient_light_duration";
     private static final String AMBIENT_LIGHT_REPEAT_COUNT = "ambient_light_repeat_count";
     private static final String AMBIENT_LIGHT_ALWAYS = "ambient_light_pulse_for_all";
-    private static final String SYNTHOS_AMBIENT_TEXT_STRING = "synthos_ambient_text_string";
-    private static final String SYNTHOS_AMBIENT_TEXT_ALIGNMENT = "synthos_ambient_text_alignment";
-    private static final String SYNTHOS_AMBIENT_TEXT_FONT = "synthos_ambient_text_font";
-    private static final String SYNTHOS_AMBIENT_TEXT_TYPE_COLOR = "synthos_ambient_text_type_color";
-    private static final String SYNTHOS_AMBIENT_TEXT_COLOR = "synthos_ambient_text_color";
-    private static final String FILE_AMBIENT_SELECT = "file_ambient_select";
-
-    private static final int REQUEST_PICK_IMAGE = 0;
 
     private SystemSettingSwitchPreference mEdgeLightAlways;
     private ListPreference mEdgeLightColorMode;
     private ColorPickerPreference mEdgeLightColor;
     private CustomSeekBarPreference mEdgeLightDuration;
     private CustomSeekBarPreference mEdgeLightRepeatCount;
-
-    private SystemSettingEditTextPreference mAmbientText;
-    private ListPreference mAmbientTextAlign;
-    private ListPreference mAmbientTextFonts;
-    private ListPreference mAmbientTextTypeColor;
-    private ColorPickerPreference mAmbientTextColor;
-
-    private Preference mAmbientImage;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -110,56 +91,6 @@ public class Ambient extends SettingsPreferenceFragment implements
         mEdgeLightRepeatCount.setValue(edgeLightRepeatCount);
         mEdgeLightRepeatCount.setOnPreferenceChangeListener(this);
 
-         mAmbientText = (SystemSettingEditTextPreference) findPreference(SYNTHOS_AMBIENT_TEXT_STRING);
-        mAmbientText.setOnPreferenceChangeListener(this);
-
-        // set ambient text alignment
-        mAmbientTextAlign = (ListPreference) findPreference(SYNTHOS_AMBIENT_TEXT_ALIGNMENT);
-        int align = Settings.System.getInt(resolver,
-                Settings.System.SYNTHOS_AMBIENT_TEXT_ALIGNMENT, 3);
-        mAmbientTextAlign.setValue(String.valueOf(align));
-        mAmbientTextAlign.setSummary(mAmbientTextAlign.getEntry());
-        mAmbientTextAlign.setOnPreferenceChangeListener(this);
-
-        // ambient text Fonts
-        mAmbientTextFonts = (ListPreference) findPreference(SYNTHOS_AMBIENT_TEXT_FONT);
-        mAmbientTextFonts.setValue(String.valueOf(Settings.System.getInt(
-                getContentResolver(), Settings.System.SYNTHOS_AMBIENT_TEXT_FONT, 8)));
-        mAmbientTextFonts.setSummary(mAmbientTextFonts.getEntry());
-        mAmbientTextFonts.setOnPreferenceChangeListener(this);
-
-        // ambient text color type
-        mAmbientTextTypeColor = (ListPreference) findPreference(SYNTHOS_AMBIENT_TEXT_TYPE_COLOR);
-        mAmbientTextTypeColor.setValue(String.valueOf(Settings.System.getInt(
-                getContentResolver(), Settings.System.SYNTHOS_AMBIENT_TEXT_TYPE_COLOR, 0)));
-        mAmbientTextTypeColor.setSummary(mAmbientTextTypeColor.getEntry());
-        mAmbientTextTypeColor.setOnPreferenceChangeListener(this);
-
-        mAmbientTextColor = (ColorPickerPreference) findPreference(SYNTHOS_AMBIENT_TEXT_COLOR);
-        mAmbientTextColor.setOnPreferenceChangeListener(this);
-        int ambientTextColor = Settings.System.getInt(getContentResolver(),
-                Settings.System.SYNTHOS_AMBIENT_TEXT_COLOR, 0xFF3980FF);
-        String ambientTextColorHex = String.format("#%08x", (0xFF3980FF & ambientTextColor));
-        if (ambientTextColorHex.equals("#ff3980ff")) {
-            mAmbientTextColor.setSummary(R.string.default_string);
-        } else {
-            mAmbientTextColor.setSummary(ambientTextColorHex);
-        }
-        mAmbientTextColor.setNewPreviewColor(ambientTextColor);
-
-        mAmbientImage = findPreference(FILE_AMBIENT_SELECT);
-
-    }
-
-       @Override
-    public boolean onPreferenceTreeClick(Preference preference) {
-        if (preference == mAmbientImage) {
-            Intent intent = new Intent(Intent.ACTION_PICK);
-            intent.setType("image/*");
-            startActivityForResult(intent, REQUEST_PICK_IMAGE);
-            return true;
-        }
-        return super.onPreferenceTreeClick(preference);
     }
 
     @Override
@@ -199,55 +130,8 @@ public class Ambient extends SettingsPreferenceFragment implements
             Settings.System.putIntForUser(getContentResolver(),
                     Settings.System.AMBIENT_LIGHT_REPEAT_COUNT, value, UserHandle.USER_CURRENT);
             return true;
-         } else if (preference == mAmbientText) {
-            String value = (String) newValue;
-            Settings.System.putString(resolver,
-                    Settings.System.SYNTHOS_AMBIENT_TEXT_STRING, value);
-            return true;
-        } else if (preference == mAmbientTextAlign) {
-            int align = Integer.valueOf((String) newValue);
-            int index = mAmbientTextAlign.findIndexOfValue((String) newValue);
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.SYNTHOS_AMBIENT_TEXT_ALIGNMENT, align);
-            mAmbientTextAlign.setSummary(mAmbientTextAlign.getEntries()[index]);
-            return true;
-        } else if (preference == mAmbientTextFonts) {
-            Settings.System.putInt(getContentResolver(), Settings.System.SYNTHOS_AMBIENT_TEXT_FONT,
-                    Integer.valueOf((String) newValue));
-            mAmbientTextFonts.setValue(String.valueOf(newValue));
-            mAmbientTextFonts.setSummary(mAmbientTextFonts.getEntry());
-            return true;
-        } else if (preference == mAmbientTextTypeColor) {
-            Settings.System.putInt(getContentResolver(), Settings.System.SYNTHOS_AMBIENT_TEXT_TYPE_COLOR,
-                    Integer.valueOf((String) newValue));
-            mAmbientTextTypeColor.setValue(String.valueOf(newValue));
-            mAmbientTextTypeColor.setSummary(mAmbientTextTypeColor.getEntry());
-            return true;
-        } else if (preference == mAmbientTextColor) {
-            String hex = ColorPickerPreference.convertToARGB(
-                    Integer.valueOf(String.valueOf(newValue)));
-            if (hex.equals("#ff3980ff")) {
-                preference.setSummary(R.string.default_string);
-            } else {
-                preference.setSummary(hex);
-            }
-            int intHex = ColorPickerPreference.convertToColorInt(hex);
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.SYNTHOS_AMBIENT_TEXT_COLOR, intHex);
-            return true;
         }
         return false;
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent result) {
-        if (requestCode == REQUEST_PICK_IMAGE) {
-            if (resultCode != Activity.RESULT_OK) {
-                return;
-            }
-            final Uri imageUri = result.getData();
-            Settings.System.putString(getContentResolver(), Settings.System.SYNTHOS_AMBIENT_CUSTOM_IMAGE, imageUri.toString());
-        }
     }
 
     private void updateColorPrefs(int edgeLightColorMode) {
